49dff7f1aacc1a42d39c94e6f5c458eeddd205fb
fella <mamechefella3@gmail.com>
2023-11-20 20:06:07 +0100

    add ws

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const WebSocket = require("ws");
 const mongoose = require("mongoose");
 const db = require("./db");
 const cors = require("cors");
@@ -6,6 +7,26 @@ const cors = require("cors");
 require("dotenv").config();
 
 const App = express();
+
+const http = require("http");
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// Set up CORS headers
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// WebSocket server logic
+wss.on("connection", (ws) => {
+  // WebSocket connection handling
+});
 
 App.use(express.json());
 App.use(cors());
