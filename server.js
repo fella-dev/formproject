9890df41aa51684bdb5f996d5663e90968142995
fella <mamechefella3@gmail.com>
2023-11-22 17:58:34 +0100

    add cors code

--- server.js
+++ server.js
@@ -11,6 +11,23 @@ const App = express();
 App.use(express.json());
 App.use(cors());
 
+const allowedOrigins = [
+  "https://gleaming-kitten-460220.netlify.app",
+  "http://localhost:4000",
+]; // Add your Netlify domain here
+
+App.use(
+  cors({
+    origin: function (origin, callback) {
+      // Check if the origin is allowed, or if it's a same-origin request (e.g., from localhost)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 db.connect();
 
 const FormsRouter = require("./form.route");
